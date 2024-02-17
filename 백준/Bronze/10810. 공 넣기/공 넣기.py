8a387b3cb60f001840270db0733222a8a694b7bd
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-02-17 11:42:55 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,12 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+ans = [0 for _ in range(n)]
+
+for a in range(m):
+    i, j, k = map(int, sys.stdin.readline().split())
+    for b in range(i, j+1):
+        ans[b-1] = k
+
+for i in range(n):
+    print(ans[i], end=" ")
