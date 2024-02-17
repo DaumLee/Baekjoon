d7dc1b27da7e1d10b002f89c55b70de1e16cf70b
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-02-17 11:04:22 +0900

    [Bronze I] Title: 평균, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+sum = 0
+score = list(map(int, sys.stdin.readline().split()))
+m = max(score)
+
+for i in range(n):
+    score[i] = score[i] / m * 100
+    sum += score[i]
+
+print(sum/(len(score)))
