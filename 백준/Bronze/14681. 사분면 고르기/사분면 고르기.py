baf3696e352afe89330dcee0f6887640b490d566
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-02-17 12:05:50 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,14 @@
+import sys
+
+a, b = map(int, sys.stdin.read().split())
+
+if a > 0:
+    if b > 0:
+        print(1)
+    else:
+        print(4)
+else:
+    if b > 0:
+        print(2)
+    else:
+        print(3)
