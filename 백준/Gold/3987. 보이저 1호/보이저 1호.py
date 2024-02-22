0b6f653d5e9fb875ffe2c0d0d857ae882709a072
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-02-22 16:41:05 +0900

    [Gold V] Title: 보이저 1호, Time: 176 ms, Memory: 120944 KB -BaekjoonHub

--- 백준/Gold/3987. 보이저 1호/보이저 1호.py
+++ 백준/Gold/3987. 보이저 1호/보이저 1호.py
@@ -1,70 +1,65 @@
 def turn(ci, cj, idx):
-    if graph[ci][cj] == '/' and idx == 2:
-        idx = 3
-    elif graph[ci][cj] == '/' and idx == 1:
-        idx = 0
-    elif graph[ci][cj] == '/' and idx == 0:
-        idx = 1
-    elif graph[ci][cj] == '/' and idx == 3:
-        idx = 2
-    elif graph[ci][cj] == '\\' and idx == 2:
-        idx = 1
-    elif graph[ci][cj] == '\\' and idx == 3:
-        idx = 0
-    elif graph[ci][cj] == '\\' and idx == 1:
-        idx = 2
-    elif graph[ci][cj] == '\\' and idx == 0:
-        idx = 3
-    return ci, cj, idx
+
+    if graph[ci][cj] == '/':
+        idx = rf1[idx]
+    elif graph[ci][cj] == '\\':
+        idx = rf2[idx]
+
+    return idx
 
 
 def solve(si, sj, i):
-    global mx, dir
+    global mx, dr
 
     idx = i
     ci, cj = si, sj
     time = 0
+
     while True:
-        if time > N*M * 2:
+        # 타임아웃이면 Voyager
+        if time > N * M * 2:
             mx = 'Voyager'
-            dir = i
+            dr = i
             return
-        # 행성에서 방향 전환
-        ci, cj, idx = turn(ci, cj, idx)
+
+        # 행성에서 방향 전환 확인
+        idx = turn(ci, cj, idx)
         ni, nj = ci+delta[idx][0], cj+delta[idx][1]
+        # 갈 수 있으면 한칸 나아감
         if 0 <= ni < N and 0 <= nj < M and graph[ni][nj] != 'C':
             ci, cj = ni, nj
             time += 1
+        # 갈 수 없으면 종료하고 최댓값 갱신
         else:
             if mx < time:
                 mx = time+1
-                dir = i
+                dr = i
             return
 
 
 N, M = map(int, input().split())
 graph = [list(input()) for _ in range(N)]
-
+# 시작점
 si, sj = map(int, input().split())
 # 위, 오른쪽, 아래, 왼쪽
 delta = ((-1, 0), (0, 1), (1, 0), (0, -1))
 # 최대 시간
 mx = 0
 # 정답 방향
-dir = 0
+dr = 0
+dct = {0: 'U', 1: 'R', 2: 'D', 3: 'L'}
 
+# / 반사
+rf1 = {0: 1, 1: 0, 2: 3, 3: 2}
+# \ 반사
+rf2 = {0: 3, 1: 2, 2: 1, 3: 0}
+
+# 4 방향 시그널 모두 시도
 for i in range(4):
     if type(mx) != str:
+        # 좌표를 (0, 0) ~ (N-1, M-1)로 맞춰줌
         solve(si-1, sj-1, i)
 
-if dir == 0:
-    dir = 'U'
-elif dir == 1:
-    dir = 'R'
-elif dir == 2:
-    dir = 'D'
-else:
-    dir = 'L'
-
-print(dir)
+# 방향은 문자로 출력
+print(dct[dr])
 print(mx)
