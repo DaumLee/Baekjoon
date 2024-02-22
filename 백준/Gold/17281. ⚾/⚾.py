cd47de602d5380f7f4342b0c31f38d152cce895c
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-02-22 20:32:38 +0900

    [Gold IV] Title: ⚾, Time: 1216 ms, Memory: 111528 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.py
+++ 백준/Gold/17281. ⚾/⚾.py
@@ -1,23 +1,9 @@
 from itertools import permutations
 
 
-# def check(lineup):
-#     # 중복체크 배열
-#     tmp = []
-#     for i in lineup:
-#         print(lineup, i)
-#         tmp.append(lst_t[i])
-#
-#     # 이미 해당 타순과 같은 결과를 낼 수 있는 게임을 진행했으면
-#     if tmp in arr:
-#         return False
-#     else:
-#         arr.append(tmp)
-#         return True
-
-
-def game(lineup):
+def play(lineup):
     lineup = list(lineup)
+    # 라인업의 4번 타자는 1번 선수
     lineup.insert(3, 0)
     global ans
 
@@ -29,69 +15,51 @@ def game(lineup):
     for i in range(N):
         # 아웃카운트
         out = 0
-        # 1, 2, 3루 베이스의 주자 초기화, base[3] = 득점
+        # 1, 2, 3루 베이스의 주자 초기화, base[0] = 득점
         base = [0, 0, 0, 0]
-        while True:
+        while out < 3:
             if lst[i][lineup[b]] == 0:
                 out += 1
             elif lst[i][lineup[b]] == 1:
-                # 3루 주자가 홈으로 이동
+                # 3루 주자 득점
                 base[3] += base[2]
-                # 2루 주자가 3루로 이동
-                base[2] = base[1]
-                # 1루 주자가 2루로 이동
-                base[1] = base[0]
-                # 출루
-                base[0] = 1
+                # 타자와 주자 진루
+                base[2], base[1], base[0] = base[1], base[0], 1
             elif lst[i][lineup[b]] == 2:
-                # 2~3루 주자가 홈으로 이동
+                # 2,3루 주자 득점
                 base[3] += base[2] + base[1]
-                # 1루 주자가 3루로 이동
-                base[2] = base[0]
-                # 출루
-                base[1] = 1
-                # 1루는 공석
-                base[0] = 0
+                # 타자와 주자 진루
+                base[2], base[1], base[0] = base[0], 1, 0
             elif lst[i][lineup[b]] == 3:
-                # 기존 주자 모두 홈으로 이동
+                # 모든 주자 득점
                 base[3] += base[2] + base[1] + base[0]
-                # 출루
-                base[2] = 1
-                # 1~2루는 공석
-                base[1] = base[0] = 0
+                # 타자 출루
+                base[2], base[1], base[0] = 1, 0, 0
             else:
-                # 타자와 주자 모두 홈으로 이동
+                # 타자와 주자 모두 득점
                 base[3] += base[2] + base[1] + base[0] + 1
                 # 모든 베이스 초기화
                 base[2] = base[1] = base[0] = 0
             # 타석이 끝나면 다음 타자가 들어오고
             b = (b+1) % 9
-            # 3아웃이 되면 다음 이닝으로 넘어가면서 홈으로 들어온 점수 카운트
-            if out == 3:
-                score += base[3]
-                break
+        # 3아웃이 되면 다음 이닝으로 넘어가면서 홈으로 들어온 점수 카운트
+        score += base[3]
 
     if ans < score:
         ans = score
         return
 
 
-def solve(lineup):
-    for i in permutations(lineup):
-        # if check(i):
-        game(i)
-
-
 N = int(input())
-# N 이닝 동안
+# 이닝의 선수 별 성적
 lst = [list(map(int, input().split())) for _ in range(N)]
+# 선수의 이닝 별 성적
 lst_t = list(zip(*lst))
-# 9명 타순 처리했는지 확인
-v = [0] * 9
-# 점수
+# 최대 점수
 ans = 0
-# 중복 체크 배열
-arr = []
-solve([1, 2, 3, 4, 5, 6, 7, 8])
+
+# 1번 선수를 제외하고 순열 작성
+for i in permutations([1, 2, 3, 4, 5, 6, 7, 8]):
+    play(i)
 
 print(ans)
