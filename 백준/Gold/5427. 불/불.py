b4986fa9b82f4f5e3306a28bcd0727a0f27dc013
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-02-28 19:33:06 +0900

    [Gold IV] Title: 불, Time: 612 ms, Memory: 238244 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -1,42 +1,36 @@
 from collections import deque
 
 
-def bfs(s, fire):
-    q = deque(fire)
-    q.append(s)
+def bfs(s):
+    q = deque(s)
     v = [[0] * M for _ in range(N)]
+    v[s[-1][0]][s[-1][1]] = 1
 
     while q:
         ci, cj = q.popleft()
-        if graph[ci][cj] == '@' and (ci == 0 or ci == N - 1 or cj == 0 or cj == M - 1):
-            graph[ci][cj] = '+'
-            return v[ci][cj] + 1
-        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            ni, nj = ci + di, cj + dj
-            if 0 <= ni < N and 0 <= nj < M and not v[ni][nj]:
-                if graph[ci][cj] == '*' and graph[ni][nj] == '.':
-                    graph[ni][nj] = '*'
-                    v[ni][nj] = v[ci][cj] + 1
-                    q.append((ni, nj))
-                if graph[ci][cj] == '@' and graph[ni][nj] == '.':
-                    graph[ni][nj] = '@'
-                    v[ni][nj] = v[ci][cj] + 1
-                    q.append((ni, nj))
+        for di, dj in ((-1, 0), (1, 0), (0, 1), (0, -1)):
+            ni, nj = ci+di, cj+dj
+            if (ni == -1 or ni == N or nj == -1 or nj == M) and board[ci][cj] == '@':
+                return v[ci][cj]
+            if 0 <= ni < N and 0 <= nj < M and not v[ni][nj] and board[ni][nj] != '#' and board[ni][nj] != '*' and board[ni][nj] != '@':
+                board[ni][nj] = board[ci][cj]
+                v[ni][nj] = v[ci][cj] + 1
+                q.append((ni, nj))
     return 'IMPOSSIBLE'
 
+
 T = int(input())
-
-for tc in range(1, T+1):
+for tc in range(T):
     M, N = map(int, input().split())
-    graph = [list(input()) for _ in range(N)]
-    s = (0, 0)
-    fire = []
-
+    board = []
+    start = []
+    magma = []
     for i in range(N):
+        lst = list(input())
+        board.append(lst)
         for j in range(M):
-            if graph[i][j] == '@':
-                s = i, j
-            elif graph[i][j] == '*':
-                fire.append((i, j))
-
-    print(bfs(s, fire))
+            if lst[j] == '@':
+                start.append((i, j))
+            elif lst[j] == '*':
+                magma.append((i, j))
+    print(bfs(magma+start))
