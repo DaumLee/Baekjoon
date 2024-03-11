f3dac8485bcfa56a91423dd4d774201678c4a7d4
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-03-11 10:34:19 +0900

    [Gold IV] Title: 지뢰찾기, Time: 124 ms, Memory: 110404 KB -BaekjoonHub

---
+++ 백준/Gold/2140. 지뢰찾기/지뢰찾기.py
@@ -0,0 +1,37 @@
+N = int(input())
+arr = []
+
+for i in range(N):
+    lst = list(input())
+    for j in range(N):
+        if lst[j] != '#':
+            lst[j] = int(lst[j])
+    arr.append(lst)
+
+corner_idx = ((1, 1), (1, N-2), (N-2, 1), (N-2, N-2))
+corner_check = ((0, 0), (0, N-1), (N-1, 0), (N-1, N-1))
+delta = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+
+ans = 0
+for c in range(4):
+    ci, cj = corner_idx[c]
+    ni, nj = corner_check[c]
+    if 0 <= ci < N and 0 <= cj < N and 0 <= ni < N and 0 <= nj < N:
+        if arr[ni][nj] > 0:
+            ans += 1
+            for di, dj in delta:
+                ni, nj = ci+di, cj+dj
+                if type(arr[ni][nj]) is int:
+                    arr[ni][nj] -= 1
+
+star = 0
+for i in range(1, N-1):
+    star += int(arr[i][0])
+    star += int(arr[0][i])
+    star += int(arr[i][N-1])
+    star += int(arr[N-1][i])
+
+if N >= 5:
+    print(ans+star//3+(N-4)**2)
+else:
+    print(ans+star//3)
