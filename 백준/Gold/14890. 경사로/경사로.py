1162f7d56cdacbdb9fd2df12c04245ed52261eef
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-04-01 10:01:18 +0900

    [Gold III] Title: 경사로, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.py
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -1,93 +1,73 @@
-def hor(n):
-    global ans
-
-    lst = []
-    for i in range(N):
-        lst.append([arr[n][i], 0])
-
-    for i in range(N-1):
-        # 경사로를 둘 수 없는 경우
-        if i+L >= N:
-            # 다음 값과 비교
-            if lst[i][0] != lst[i+1][0]:
-                return
-        # 경사로를 둘 수 있는 경우
-        else:
-            # 내리막
-            if lst[i][0] > lst[i+L][0] and lst[i][0] != lst[i+1][0]:
-                for j in range(i+1, i+L+1):
-                    # 이미 경사로가 있으면
-                    if lst[j][1]:
-                        return
-                    # 값이 연속하지 않으면
-                    if lst[j][0]+1 != lst[i][0]:
-                        return
-                    lst[j][1] = 1
-            # 오르막
-            elif lst[i][0] < lst[i+L][0]:
-                for j in range(i, i+L):
-                    # 이미 경사로가 있으면
-                    if lst[j][1]:
-                        return
-                    # 경사로를 탔을 때 값이 목적지와 다르면
-                    if lst[j][0]+1 != lst[i+L][0]:
-                        return
-                    lst[j] = [lst[j][0]+1, 1]
-            # 범위는 가능하지만 값이 같은 경우
+def build(val, ni, nj, col=0):
+    # 상하 활주로 건설
+    if col:
+        if ni < 0 or ni+X-1 >= N:
+            return False
+        for i in range(ni, ni+X):
+            if arr[i][nj] != val:
+                return False
+            if v[i][nj] != num:
+                v[i][nj] = num
             else:
-                if lst[i][0] != lst[i+1][0]:
-                    return
-    ans += 1
-
-
-def ver(n):
-    global ans
-
-    lst = []
-    for i in range(N):
-        lst.append([arr[i][n], 0])
-
-    for i in range(N-1):
-        # 경사로를 둘 수 없는 경우
-        if i+L >= N:
-            # 다음 값과 비교
-            if lst[i][0] != lst[i+1][0]:
-                return
-        # 경사로를 둘 수 있는 경우
-        else:
-            # 내리막
-            if lst[i][0] > lst[i+L][0] and lst[i][0] != lst[i+1][0]:
-                for j in range(i+1, i+L+1):
-                    # 이미 경사로가 있으면
-                    if lst[j][1]:
-                        return
-                    # 값이 연속하지 않으면
-                    if lst[j][0]+1 != lst[i][0]:
-                        return
-                    lst[j][1] = 1
-            # 오르막
-            elif lst[i][0] < lst[i+L][0]:
-                for j in range(i, i+L):
-                    # 이미 경사로가 있으면
-                    if lst[j][1]:
-                        return
-                    # 경사로를 탔을 때 값이 목적지와 다르면
-                    if lst[j][0]+1 != lst[i+L][0]:
-                        return
-                    lst[j] = [lst[j][0]+1, 1]
-            # 범위는 가능하지만 값이 같은 경우
+                return False
+    # 좌우 활주로 건설
+    else:
+        if nj < 0 or nj+X-1 >= N:
+            return False
+        for j in range(nj, nj+X):
+            if arr[ni][j] != val:
+                return False
+            if v[ni][j] != num:
+                v[ni][j] = num
             else:
-                if lst[i][0] != lst[i+1][0]:
-                    return
-    ans += 1
+                return False
+    return True
 
 
-N, L = map(int, input().split())
+def check(si, sj):
+    if si >= 0:
+        line = arr[si][:]
+        for j in range(N-1):
+            if line[j] == line[j+1]:
+                continue
+            # 내려가는 활주로
+            elif line[j] == line[j+1]+1:
+                if not build(line[j+1], si, j+1):
+                    return 0
+            # 올라가는 활주로
+            elif line[j] == line[j+1]-1:
+                if not build(line[j], si, j-X+1):
+                    return 0
+            else:
+                return 0
+    else:
+        line = []
+        for i in range(N):
+            line.append(arr[i][sj])
+        for j in range(N-1):
+            if line[j] == line[j+1]:
+                continue
+            # 내려가는 활주로
+            elif line[j] == line[j+1]+1:
+                if not build(line[j+1], j+1, sj, 1):
+                    return 0
+            # 올라가는 활주로
+            elif line[j] == line[j+1]-1:
+                if not build(line[j], j-X+1, sj, 1):
+                    return 0
+            else:
+                return 0
+    return True
+
+N, X = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+v = [[0] * N for _ in range(N)]
+# v를 색칠할 번호
+num = 1
 ans = 0
-
 for i in range(N):
-    hor(i)
-    ver(i)
-
+    ans += check(i, -1)
+    num += 1
+    ans += check(-1, i)
+    num += 1
 print(ans)
