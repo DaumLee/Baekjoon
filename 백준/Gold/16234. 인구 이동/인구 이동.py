a847b1093dc36c30520a879c0430aefbfc752d79
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-04-01 11:28:22 +0900

    [Gold IV] Title: 인구 이동, Time: 656 ms, Memory: 126368 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,50 +1,44 @@
-from collections import deque
+def find(v):
+    if p[v] >= 0:
+        p[v] = find(p[v])
+    else:
+        return v
+    return p[v]
 
 
-def bfs(si, sj, t):
-    q = deque([(si, sj)])
-    cnt = 1
-    sm = arr[si][sj]
-
-    while q:
-        ci, cj = q.popleft()
-        for di, dj in delta:
-            ni, nj = ci+di, cj+dj
-            # 국경선을 열 수 있으면
-            if 0 <= ni < N and 0 <= nj < N and not v[ni][nj] and L <= abs(arr[ci][cj]-arr[ni][nj]) <= R:
-                cnt += 1
-                sm += arr[ni][nj]
-                v[ni][nj] = t
-                q.append((ni, nj))
-                nlst[t].append((ni, nj))
-    return sm, cnt
+def union(a, b):
+    A, B = find(a), find(b)
+    if p[A] >= p[B]:
+        p[B] += p[A]
+        p[A] = B
+        sm[B] += sm[A]
+    else:
+        p[A] += p[B]
+        p[B] = A
+        sm[A] += sm[B]
 
 
 N, L, R = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-delta = ((-1, 0), (0, -1), (0, 1), (1, 0))
 time = 0
-dct = dict()
-
-while time <= 2000:
-    t = 0
-    v = [[0] * N for _ in range(N)]
-    nlst = dict()
+while True:
+    p = [-1] * N**2
+    sm = {num: arr[num//N][num%N] for num in range(N**2)}
     for i in range(N):
         for j in range(N):
-            if not v[i][j]:
-                t += 1
-                v[i][j] = t
-                nlst[t] = [(i, j)]
-                dct[t] = bfs(i, j, t)
-
-    # 국경선을 열 수 없으면
-    if t == N**2:
+            if i+1 < N:
+                if L <= abs(arr[i][j]-arr[i+1][j]) <= R:
+                    if find(i*N+j) != find((i+1)*N+j):
+                        union(i*N+j, (i+1)*N+j)
+            if j+1 < N:
+                if L <= abs(arr[i][j]-arr[i][j+1]) <= R:
+                    if find(i*N+j) != find(i*N+j+1):
+                        union(i*N+j, i*N+j+1)
+    if sum(p) == -N**2:
         break
-    for n in range(1, t+1):
-        sm, cnt = dct[n]
-        for i, j in nlst[n]:
-            arr[i][j] = sm//cnt
+    for num in range(N**2):
+        cnt = -p[find(num)]
+        val = sm[find(num)]
+        arr[num//N][num%N] = val//cnt
     time += 1
-
 print(time)
