2f53e14cc7d8d322f8bcae0466d3d8e3321c1787
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-04-01 12:55:23 +0900

    [Gold III] Title: 나무 재테크, Time: 680 ms, Memory: 128048 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,64 +1,49 @@
 from collections import deque
 
-def earn(cur):
-    nxt = [[deque() for j in range(N)] for i in range(N)]
-
-    # 봄
-    for i in range(N):
-        for j in range(N):
-            # 나무가 있으면
-            if cur[i][j]:
-                for idx in range(len(cur[i][j])):
-                    age = cur[i][j][idx]
-                    # 양분 체크 후 다음 연도에 추가
-                    if arr[i][j] >= age:
-                        arr[i][j] -= age
-                        nxt[i][j].append(age+1)
-                    # 양분이 다 떨어졌으면 여름 진행
-                    else:
-                        for a in range(idx, len(cur[i][j])):
-                            age = cur[i][j][a]
-                            arr[i][j] += age//2
-                        break
-
-    # 가을
-    for i in range(N):
-        for j in range(N):
-            # 나무가 있으면
-            if nxt[i][j]:
-                for age in nxt[i][j]:
-                    # 나이가 5의 배수면
-                    if age%5 == 0:
-                        for di, dj in delta:
-                            ni, nj = i+di, j+dj
-                            # 맨 앞에 나이가 1인 나무 추가
-                            if 0 <= ni < N and 0 <= nj < N:
-                                nxt[ni][nj].appendleft(1)
-
-    #  겨울
-    for i in range(N):
-        for j in range(N):
-            arr[i][j] += A[i][j]
-
-    return nxt
-
 N, M, K = map(int, input().split())
+virus = [[deque() for _ in range(N)] for _ in range(N)]
+delta = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+plus = [list(map(int, input().split())) for _ in range(N)]
 arr = [[5] * N for _ in range(N)]
-A = [list(map(int, input().split())) for _ in range(N)]
-tree = [[deque() for j in range(N)] for i in range(N)]
-
-delta = ((-1, 0), (-1, 1), (-1, -1), (0, 1), (0, -1), (1, -1), (1, 1), (1, 0))
 
 for _ in range(M):
-    i, j, k = map(int, input().split())
-    tree[i-1][j-1].append(k)
+    si, sj, age = map(int, input().split())
+    si, sj = si-1, sj-1
+    virus[si][sj].append(age)
 
 for _ in range(K):
-    tree = earn(tree)
+    for i in range(N):
+        for j in range(N):
+            if virus[i][j]:
+                nxt = deque()
+                while virus[i][j]:
+                    v = virus[i][j].popleft()
+                    if arr[i][j] >= v:
+                        arr[i][j] -= v
+                        nxt.append(v+1)
+                    else:
+                        arr[i][j] += v // 2
+                        while virus[i][j]:
+                            v = virus[i][j].popleft()
+                            arr[i][j] += v//2
+                        break
+                virus[i][j] = nxt
+    for i in range(N):
+        for j in range(N):
+            if virus[i][j]:
+                for idx in range(len(virus[i][j])):
+                    tree = virus[i][j][idx]
+                    if tree%5 == 0:
+                        for di, dj in delta:
+                            ni, nj = i+di, j+dj
+                            if 0 <= ni < N and 0 <= nj < N:
+                                virus[ni][nj].appendleft(1)
+    for i in range(N):
+        for j in range(N):
+            arr[i][j] += plus[i][j]
 
-sm = 0
+ans = 0
 for i in range(N):
     for j in range(N):
-        sm += len(tree[i][j])
-
-print(sm)
+        ans += len(virus[i][j])
+print(ans)
