cbe09e07f5328a69539bdf79626ffdb706037c5f
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-04-01 14:37:51 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 1048 ms, Memory: 127248 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,95 +1,66 @@
-def up(si):
-    ci, cj = ref[0][0]-1, 0
-    for d in range(4):
-        di, dj = delta[d]
-        while True:
-            ni, nj = ci+di, cj+dj
-            if d == 0 and ni < 0:
-                break
-            elif d == 1 and nj > M-1:
-                break
-            elif d == 2 and ni > si:
-                break
-            elif d == 3 and nj < 0:
-                break
-            arr[ci][cj] = arr[ni][nj]
-            ci, cj = ni, nj
-
-
-def down(si):
-    ci, cj = ref[1][0]+1, 0
-    for d in range(3,-1,-1):
-        d = (d-1)%4
-        di, dj = delta[d]
-        while True:
-            ni, nj = ci+di, cj+dj
-            if d == 0 and ni < si:
-                break
-            elif d == 1 and nj > M-1:
-                break
-            elif d == 2 and ni > N-1:
-                break
-            elif d == 3 and nj < 0:
-                break
-            arr[ci][cj] = arr[ni][nj]
-            ci, cj = ni, nj
-
-
-def bfs(d):
-    # 확산될 미세먼지
-    v = [[0] * M for _ in range(N)]
-
-    for ci, cj in d:
-        cnt = 0
-        for di, dj in ((-1, 0), (1, 0), (0, 1), (0, -1)):
-            ni, nj = ci+di, cj+dj
-            # 공청기가 없는 칸으로 확산
-            if 0 <= ni < N and 0 <= nj < M and (ni, nj) not in ref:
-                v[ni][nj] += arr[ci][cj]//5
-                cnt += 1
-        v[ci][cj] -= (arr[ci][cj]//5)*cnt
-
-    # 최종 미세먼지 동시 확산 처리
+def spread():
+    nxt = [[0] * M for _ in range(N)]
     for i in range(N):
         for j in range(M):
-            arr[i][j] += v[i][j]
+            if arr[i][j] > 0:
+                cnt = 0
+                for di, dj in delta:
+                    ni, nj = i+di, j+dj
+                    if 0 <= ni < N and 0 <= nj < M and (ni, nj) not in hos:
+                        cnt += 1
+                        nxt[ni][nj] += arr[i][j]//5
+                nxt[i][j] += arr[i][j]-((arr[i][j]//5) * cnt)
+    return nxt
+
+
+def upside(start, cd):
+    ci, cj = start
+    while True:
+        di, dj = delta[cd]
+        ni, nj = ci+di, cj+dj
+        if (ni, nj) == start:
+            break
+        if 0 <= ni <= start[0] and 0 <= nj < M:
+            arr[ci][cj], arr[ni][nj] = arr[ni][nj], arr[ci][cj]
+            ci, cj = ni, nj
+        else:
+            cd = (cd-1)%4
+    arr[start[0]][start[1]] = 0
+
+
+def downside(start, cd):
+    ci, cj = start
+    while True:
+        di, dj = delta[cd]
+        ni, nj = ci+di, cj+dj
+        if (ni, nj) == start:
+            break
+        if start[0] <= ni < N and 0 <= nj < M:
+            arr[ci][cj], arr[ni][nj] = arr[ni][nj], arr[ci][cj]
+            ci, cj = ni, nj
+        else:
+            cd = (cd+1)%4
+    arr[start[0]][start[1]] = 0
 
 
 N, M, T = map(int, input().split())
 arr = []
-ref = []
-dust = []
+hos = []
 for i in range(N):
     lst = list(map(int, input().split()))
     for j in range(M):
-        # 공청기
         if lst[j] == -1:
-            ref.append((i, j))
+            hos.append((i, j))
             lst[j] = 0
-        # 미세먼지
-        elif lst[j]:
-            dust.append((i, j))
     arr.append(lst)
 
-# 위, 오른쪽, 아래, 왼쪽
-delta = ((-1, 0), (0, 1), (1, 0), (0, -1))
-
+delta = ((0, 1), (-1, 0), (0, -1), (1, 0))
 for _ in range(T):
-    # 미세먼지 확산
-    bfs(dust)
-    arr[ref[0][0]-1][0] = 0
-    arr[ref[1][0]+1][0] = 0
-    # 공기청정기
-    up(ref[0][0])
-    down(ref[1][0])
-    dust = []
-    for i in range(N):
-        for j in range(M):
-            if arr[i][j]:
-                dust.append((i, j))
-
-sm = 0
+    arr = spread()
+    upside(hos[0], 1)
+    downside(hos[1], 3)
+ans = 0
 for i in range(N):
-    sm += sum(arr[i])
-
-print(sm)
+    for j in range(M):
+        ans += arr[i][j]
+print(ans)
