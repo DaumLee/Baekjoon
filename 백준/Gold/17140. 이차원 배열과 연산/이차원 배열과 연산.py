1a77ef61d9920c20191f81b03d1b8e1bd7546cea
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-04-02 10:06:02 +0900

    [Gold IV] Title: 이차원 배열과 연산, Time: 144 ms, Memory: 110756 KB -BaekjoonHub

--- 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.py
+++ 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.py
@@ -1,73 +1,43 @@
-def R(A):
-    new_A = []
-    # 최대 길이
+def sorting():
+    nxt = []
     mx = 0
-    for i in range(len(A)):
-        cnts = [0] * 101
-        lst = A[i]
-        for i in lst:
-            cnts[i] += 1
-        new_lst = []
-        for v in range(1, 101):
-            for c in range(1, 101):
-                # 0개는 확인하지 않고
-                if cnts[c] == 0: continue
-                # 최솟값은 크기 순으로 값, 개수를 new_lst에 추가
-                if cnts[c] == v:
-                    new_lst.append(c)
-                    new_lst.append(v)
-        mx = max(mx, len(new_lst[:101]))
-        new_A.append(new_lst[:101])
-    for i in range(len(new_A)):
-        if len(new_A[i]) < mx:
-            new_A[i] = new_A[i] + [0]*(mx - len(new_A[i]))
-    return new_A
+    for lst in arr:
+        chk = []
+        ret = []
+        for i in range(len(lst)):
+            if lst[i] and lst[i] not in chk:
+                chk.append(lst[i])
+                ret.append([lst[i], lst.count(lst[i])])
+        ret.sort(key=lambda x: (x[1], x[0]))
+        nxt.append([])
+        for item in ret:
+            nxt[-1].extend(item)
+        nxt[-1] = nxt[-1][:100]
+        mx = max(mx, len(nxt[-1]))
+    for i in range(len(nxt)):
+        if len(nxt[i]) < mx:
+            nxt[i] += [0]*(mx-len(nxt[i]))
+    return nxt
 
 
-def C(A):
-    new_A = []
-    # 최대 길이
-    mx = 0
-    for j in range(len(A[0])):
-        cnts = [0] * 101
-        for i in range(len(A)):
-            cnts[A[i][j]] += 1
-        new_lst = []
-        for v in range(1, 101):
-            for c in range(1, 101):
-                # 0개는 확인하지 않고
-                if cnts[c] == 0: continue
-                # 최솟값은 크기 순으로 값, 개수를 new_lst에 추가
-                if cnts[c] == v:
-                    new_lst.append(c)
-                    new_lst.append(v)
-        mx = max(mx, len(new_lst[:101]))
-        new_A.append(new_lst[:101])
-    for i in range(len(new_A)):
-        if len(new_A[i]) < mx:
-            new_A[i] = new_A[i] + [0]*(mx - len(new_A[i]))
-
-    ret_A = [[0] * len(new_A) for _ in range(len(new_A[0]))]
-    for i in range(len(new_A)):
-        for j in range(len(new_A[0])):
-            ret_A[j][i] = new_A[i][j]
-    return ret_A
-
-ei, ej, k = map(int, input().split())
-ei -= 1
-ej -= 1
-A = [list(map(int, input().split())) for _ in range(3)]
-
+ei, ej, K = map(int, input().split())
+ei, ej = ei-1, ej-1
+arr = [list(map(int, input().split())) for _ in range(3)]
 time = 0
-while True:
-    if ei < len(A) and ej < len(A[0]) and A[ei][ej] == k:
-        print(time)
-        break
-    if time > 100:
-        print(-1)
-        break
-    if len(A) >= len(A[0]):
-        A = R(A)
-    else:
-        A = C(A)
+while time < 101:
+    # 범위 체크
+    if ei < len(arr) and ej < len(arr[0]):
+        if arr[ei][ej] == K:
+            break
     time += 1
+    if len(arr) < len(arr[0]):
+        arr = list(map(list, zip(*arr)))[::-1]
+        arr = sorting()
+        arr = list(map(list, zip(*arr[::-1])))
+    else:
+        arr = sorting()
+    continue
+if time == 101:
+    print(-1)
+else:
+    print(time)
