0d3f3f143b66ce6f2a78ae8ba2d8d74b6460f5a8
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-04-02 10:25:16 +0900

    [Gold III] Title: 연구소 3, Time: 236 ms, Memory: 114716 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -1,78 +1,57 @@
 from collections import deque
 
-
-def spread(vrs):
-    q = deque(vrs)
-    v = [[0] * N for _ in range(N)]
-    # 최초 방문 표시
-    for vi, vj in vrs:
-        v[vi][vj] = 1
-
-    safe_cnt = safe
-    time = 0
+def check(start, num):
+    for si, sj, _ in start:
+        v[si][sj] = num
+    q = deque(start)
+    cnt = 0
     while q:
-        ci, cj = q.popleft()
-        # 바이러스만 남았으면
-        if not safe_cnt:
-            break
-        for di, dj in ((-1, 0), (1, 0), (0, 1), (0, -1)):
+        ci, cj, time = q.popleft()
+        for di, dj in delta:
             ni, nj = ci+di, cj+dj
-            if 0 <= ni < N and 0 <= nj < N and not v[ni][nj] and arr[ni][nj] != 1:
-                q.append((ni, nj))
-                v[ni][nj] = v[ci][cj]+1
-                time = max(time, v[ni][nj])
-                # 안전 영역을 바이러스로 채웠으면
-                if arr[ni][nj] != 2:
-                    safe_cnt -= 1
-
-    for i in range(N):
-        for j in range(N):
-            # 벽이 아닌데 방문한 적 없으면 불가능
-            if arr[i][j] == 0 and not v[i][j]:
-                return -1
-    return time-1
+            if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] != 1 and v[ni][nj] != num:
+                v[ni][nj] = num
+                if not arr[ni][nj]:
+                    cnt += 1
+                    if cnt == virus:
+                        return time+1
+                q.append((ni, nj, time+1))
+    return N**2+1
 
 
-def combinations(n, lst):
-    global ans
+def find(n, lst):
+    global ans, num
 
     if len(lst) == M:
-        t = spread(lst)
-        # 지금 불가능인데, 정답이 나온 적 없으면
-        if t == -1 and ans == 10e6:
-            ans = -1
-        # 정답이 지금 시간보다 더 걸렸으면
-        elif t != -1 and ans > t:
-            ans = t
-        # 기존에는 불가능이었으나 정답을 찾았다면
-        elif ans == -1 and t >= 0:
-            ans = t
-        return
-    if n == len(virus):
+        ans = min(ans, check(lst, num))
+        num += 1
         return
 
-    for i in range(n+1, len(virus)):
-        combinations(i, lst+[virus[i]])
+    for i in range(n+1, len(hp)):
+        find(i, lst+[hp[i]])
 
 
+delta = ((-1, 0), (1, 0), (0, 1), (0, -1))
 N, M = map(int, input().split())
+v = [[0] * N for _ in range(N)]
+num = 1
 arr = []
-virus = []
-# 빈칸(0) 개수
-safe = 0
+hp = []
+virus = 0
 for i in range(N):
     lst = list(map(int, input().split()))
-    arr.append(lst)
     for j in range(N):
-        if lst[j] == 2:
-            virus.append((i, j))
         if lst[j] == 0:
-            safe += 1
-
-# 빈 칸이 없으면
-if not safe:
+            virus += 1
+        if lst[j] == 2:
+            hp.append((i, j, 0))
+    arr.append(lst)
+if not virus:
     print(0)
 else:
-    ans = 10e6
-    combinations(-1, [])
-    print(ans)
+    ans = N ** 2 + 1
+    find(-1, [])
+    if ans == N**2+1:
+        print(-1)
+    else:
+        print(ans)
