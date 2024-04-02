9f6cc015bc13bfa59e1451eff3043056869ca691
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-04-02 15:26:21 +0900

    [Gold II] Title: 모노미노도미노 2, Time: 288 ms, Memory: 115408 KB -BaekjoonHub

--- 백준/Gold/20061. 모노미노도미노 2/모노미노도미노 2.py
+++ 백준/Gold/20061. 모노미노도미노 2/모노미노도미노 2.py
@@ -1,144 +1,86 @@
-def on_blue(t, x):
-    global ans, blue
-    if t == 1:
-        bj = 0
-        for j in range(6):
-            # 해당 칸이 비었으면
-            if not blue[x][j]:
-                bj = j
-            else:
-                break
-        blue[x][bj] = 1
-    elif t == 2:
-        bj = 0
-        for j in range(5):
-            # 해당 칸이 비었으면
-            if not blue[x][j+1] and not blue[x][j]:
-                bj = j
-            else:
-                break
-        blue[x][bj] = blue[x][bj+1] = 1
-    else:
-        bj = 0
-        for j in range(6):
-            # 해당 칸이 비었으면
-            if not blue[x+1][j] and not blue[x][j]:
-                bj = j
-            else:
-                break
-        blue[x+1][bj] = blue[x][bj] = 1
-    # 점수 계산을 위해 회전
-    blue = list(map(list, zip(*blue[::-1])))
-    # 점수 획득 줄
-    score_line = []
+def check(arr):
+    global ans
+    remove = []
     for i in range(2, 6):
-        if sum(blue[i]) == 4:
-            score_line.append(i)
+        if sum(arr[i]) == 4:
+            remove.append(i)
             ans += 1
             for j in range(4):
-                blue[i][j] = 0
-    # 점수 먹고 줄 내리기
-    for k in score_line:
+                arr[i][j] = 0
+    for k in remove:
         for i in range(k-1, -1, -1):
             for j in range(4):
-                blue[i][j], blue[i+1][j] = 0, blue[i][j]
-    # 연한 칸 확인
+                if arr[i][j]: arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
     cnt = 0
     for i in range(2):
-        # 연한 칸에 있으면 +1
-        if sum(blue[i]) >= 1:
+        if sum(arr[i]):
             cnt += 1
-    # 연한 칸 처리
-    if cnt >= 1:
-        # 아래는 지우고
-        for i in range(5, 5 - cnt, -1):
+    for i in range(5, 5-cnt, -1):
+        for j in range(4):
+            arr[i][j] = 0
+    for _ in range(cnt):
+        for i in range(4, -1, -1):
             for j in range(4):
-                blue[i][j] = 0
-        # 위는 내리고
-        for i in range(5 - cnt, -1, -1):
-            for j in range(4):
-                blue[i][j], blue[i+cnt][j] = 0, blue[i][j]
-    # 원상 복구
-    blue = list(map(list, zip(*blue)))[::-1]
+                if arr[i][j]: arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
+    return arr
 
 
-def on_green(t, y):
-    global ans, green
+def play(arr, t, y):
     if t == 1:
-        gi = 0
-        for i in range(6):
-            if not green[i][y]:
-                gi = i
-            else:
-                break
-        green[gi][y] = 1
+        blocks = [(0, y)]
     elif t == 2:
-        gi = 0
-        for i in range(6):
-            if not green[i][y] and not green[i][y+1]:
-                gi = i
-            else:
-                break
-        green[gi][y] = green[gi][y+1] = 1
+        blocks = [(0, y), (0, y+1)]
     else:
-        gi = 0
-        for i in range(5):
-            if not green[i][y] and not green[i+1][y]:
-                gi = i
-            else:
+        blocks = [(1, y), (0, y)]
+    flag = 0
+    for i in range(6):
+        for bi, bj in blocks:
+            # t==3일 때 블록이 세워질 수 없으면 stop
+            if i+bi > 5:
+                ei = i-1
+                flag = 1
                 break
-        green[gi][y] = green[gi+1][y] = 1
-
-    # 점수 획득 줄
-    score_line = []
-    # 점수 계산
-    for i in range(2, 6):
-        if sum(green[i]) == 4:
-            score_line.append(i)
-            ans += 1
-            for j in range(4):
-                green[i][j] = 0
-    # 점수 먹고 줄 내리기
-    for k in score_line:
-        for i in range(k-1, -1, -1):
-            for j in range(4):
-                green[i][j], green[i+1][j] = 0, green[i][j]
-    # 연한 칸 확인
-    cnt = 0
-    for i in range(2):
-        # 연한 칸에 있으면 +1
-        if sum(green[i]) >= 1:
-            cnt += 1
-    # 연한 칸 처리
-    if cnt >= 1:
-        # 아래는 지우고
-        for i in range(5, 5-cnt, -1):
-            for j in range(4):
-                green[i][j] = 0
-        # 위는 내리고
-        for i in range(5-cnt, -1, -1):
-            for j in range(4):
-                green[i][j], green[i+cnt][j] = 0, green[i][j]
+            if arr[i+bi][bj]:
+                ei = i-1
+                flag = 1
+                break
+        if flag: break
+    else:
+        ei = 5
+    if t == 1:
+        x, y = blocks[0]
+        arr[ei][y] = 1
+    elif t == 2:
+        for x, y in blocks:
+            arr[ei][y] = 1
+    else:
+        for x, y in blocks:
+            arr[ei][y] = 1
+            ei += 1
+    return arr
 
 
-N = int(input())
-
-blue = [[0] * 6 for _ in range(4)]
-green =[[0] * 4 for _ in range(6)]
-
+K = int(input())
+red = [[0] * 4 for _ in range(6)]
+yellow = [[0] * 4 for _ in range(6)]
 ans = 0
-for _ in range(N):
+for _ in range(K):
     t, x, y = map(int, input().split())
-    on_blue(t, x)
-    on_green(t, y)
+    if t == 1:
+        red = play(red, t, 3-x)
+        yellow = play(yellow, t, y)
+    elif t == 2:
+        red = play(red, t^1, 3-x)
+        yellow = play(yellow, t, y)
+    else:
+        red = play(red, t^1, 2-x)
+        yellow = play(yellow, t, y)
+    red = check(red)
+    yellow = check(yellow)
 print(ans)
-blocks = 0
-for i in range(4):
-    for j in range(6):
-        if blue[i][j]:
-            blocks += 1
+cnt = 0
 for i in range(6):
     for j in range(4):
-        if green[i][j]:
-            blocks += 1
-print(blocks)
+        if red[i][j]: cnt += 1
+        if yellow[i][j]: cnt += 1
+print(cnt)
