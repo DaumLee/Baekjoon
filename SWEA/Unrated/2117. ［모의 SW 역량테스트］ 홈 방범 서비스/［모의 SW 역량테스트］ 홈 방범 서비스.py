4eaa1404b965a272261953cb31fb7015d138e406
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-04-08 15:40:14 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 670 ms, Memory: 54,576 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.py
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.py
@@ -1,26 +1,27 @@
 from collections import deque
 
 
-def bfs(si, sj, k):
+def bfs(si, sj):
+    global ans
     q = deque([(si, sj)])
     v = [[0] * N for _ in range(N)]
     v[si][sj] = 1
-    cnt = 0
-    if arr[si][sj]:
-        cnt += 1
+    cnt = arr[si][sj]
+    k = 1
     while q:
+        k += 1
+        if k == 22: break
         for _ in range(len(q)):
             ci, cj = q.popleft()
-            if v[ci][cj] >= k:
-                return cnt
-            for di, dj in ((-1, 0), (0, 1), (1, 0), (0, -1)):
+            for di, dj in ((-1, 0), (1, 0), (0, 1), (0, -1)):
                 ni, nj = ci+di, cj+dj
                 if 0 <= ni < N and 0 <= nj < N and not v[ni][nj]:
-                    v[ni][nj] = v[ci][cj] + 1
+                    v[ni][nj] = 1
                     q.append((ni, nj))
                     if arr[ni][nj]:
                         cnt += 1
-    return cnt
+        if cnt*M >= cost[k]:
+            ans = max(ans, cnt)
 
 
 cost = {2: 5, 3: 13, 4: 25, 5: 41, 6: 61, 7: 85, 8: 113, 9: 145, 10: 181, 11: 221, 12: 265, 13: 313, 14: 365, 15: 421, 16: 481, 17: 545, 18: 613, 19: 685, 20: 761, 21: 841}
@@ -29,10 +30,7 @@ for tc in range(1, int(input())+1):
     arr = [list(map(int, input().split())) for _ in range(N)]
     ans = 1
     flag = 0
-    for K in range(2, min(22, N+2)):
-        for i in range(N):
-            for j in range(N):
-                cnt = bfs(i, j, K)
-                if cnt*M >= cost[K]:
-                    ans = max(ans, cnt)
+    for i in range(N):
+        for j in range(N):
+            bfs(i, j)
     print(f'#{tc} {ans}')
