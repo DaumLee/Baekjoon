e300ac2225100d2df725d25297c326116af020fc
DaumLee <58494226+DaumLee@users.noreply.github.com>
2024-06-21 08:58:36 +0900

    [Bronze V] Title: 2033년 밈 투표, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.py
@@ -0,0 +1,13 @@
+lst = ['Never gonna give you up',
+       'Never gonna let you down',
+       'Never gonna run around and desert you',
+       'Never gonna make you cry',
+       'Never gonna say goodbye',
+       'Never gonna tell a lie and hurt you',
+       'Never gonna stop']
+
+for _ in range(int(input())):
+    if input() not in lst:
+        print('Yes')
+        exit(0)
+print('No')
